628aba44820ed1a644489b52da48ab1dd33e0307
Joel <joel.pietilainen@helsinki.fi>
2024-11-08 23:32:12 +0200

    updates

--- frontend/src/apolloClient.js
+++ frontend/src/apolloClient.js
@@ -1,8 +1,12 @@
 import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
+import { GRAPHQL_URI } from '@env';
+
+// Load environment variables
+//dotenv.config();
 
 const client = new ApolloClient({
     link: new HttpLink({
-        uri: 'http://10.87.2.169:8000/graphql/',
+        uri: GRAPHQL_URI,//"http://192.168.252.13:8080/graphql/", // Access the URI from the .env file
     }),
     cache: new InMemoryCache(),
 });
