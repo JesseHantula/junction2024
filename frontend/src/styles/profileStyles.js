bad791e6fbb62c0eb829d1a10e0d15f83dc5d23e
Arthur Gerster <gersterarthur@icloud.com>
2024-11-09 15:06:23 +0200

    filled profile

---
+++ frontend/src/styles/profileStyles.js
@@ -0,0 +1,22 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+    container: {
+      padding: 20,
+      backgroundColor: '#f4f4f4',
+      flex: 1
+    },
+    title: {
+      fontSize: 24,
+      fontWeight: 'bold',
+      marginBottom: 20,
+      color: '#333'
+    },
+    field: {
+      fontSize: 16,
+      marginBottom: 10,
+      color: '#666'
+    }
+})
+
+export default styles
